27467767865990fa1b48c6e169f551eb53b81571
JuanVille15 <juanville9915@gmail.com>
2026-08-28 14:49:21 -0500

    Consulta Informacion SIPAS: Valor_capitalizado, Perseverancia

---
+++ sql/sipas.sql
@@ -0,0 +1,10 @@
+SELECT
+	strIdentificacion, 
+	SUM(Valor_Acum_Capitalizado) AS [Valor_Capitalizado],
+	CAST(DATEDIFF(MONTH,  CAST(GETDATE() AS DATE), MAX(dtmFechaPerseverancia)) AS INT) AS [Meses_Hasta_Perseverancia]
+FROM Operaciones.Solidaridad.tblPlanesSolidaridadAsociadosInactivos
+WHERE
+    Operaciones.$partition.pf_mes(dtmFechaInsercion) =
+    Operaciones.$partition.pf_mes('?')
+GROUP BY
+	strIdentificacion;
