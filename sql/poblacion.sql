8ce0ef7dddb53eedab9b3cb8f9ba685779391128
JuanVille15 <juanville9915@gmail.com>
2026-08-31 08:17:27 -0500

    Consulta poblacion Inactivos --- corriente

---
+++ sql/poblacion.sql
@@ -0,0 +1,16 @@
+
+SELECT  
+	fct.stridentificacion, 
+	fct.strcodestadoclientefuente, 
+	dime.strDescEstadoFuente 
+FROM BodegaCorporativa.[bodega].[factasociatividad] fct 
+INNER JOIN BodegaCorporativa.bodega.dimestado dime
+ON 
+	fct.skEstadoCliente = dime.skEstado
+	AND dime.numCodTipoEstado = 1
+	AND dime.strFuente ='CooTaylor'
+	AND dime.strDescEstadoFuente ='Inactivo'
+WHERE
+	BodegaCorporativa.$partition.Pf_mes(fct.dtmfechainsercion) =  BodegaCorporativa.$partition.pf_mes('?')
+	AND fct.[indregistroactual] = 1 
+	AND fct.strfuente = 'CooTaylor';
